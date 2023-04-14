2050604e27f213dcb0719cd4d570db3c8bd33888
MeHzqq <130269820+MeHzqq@users.noreply.github.com>
2023-04-14 19:16:34 +0930

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,22 @@
+const resizer = document.querySelector('.resizer');
+const twitch = document.querySelector('.twitch');
+const batpersonChat = document.querySelector('.batperson-chat');
+
+resizer.addEventListener('mousedown', (e) => {
+  e.preventDefault();
+  document.addEventListener('mousemove', resize);
+  document.addEventListener('mouseup', stopResize);
+});
+
+function resize(e) {
+  const containerWidth = e.clientX;
+  const chatWidth = window.innerWidth - containerWidth;
+
+  twitch.style.width = `${containerWidth}px`;
+  batpersonChat.style.width = `${chatWidth}px`;
+}
+
+function stopResize() {
+  document.removeEventListener('mousemove', resize);
+  document.removeEventListener('mouseup', stopResize);
+}
